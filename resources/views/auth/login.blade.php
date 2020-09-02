c3f361eeea70ffb00abbb8ee59fa0b24587802fd
Mesin Kasir <52827888+mesinkasir@users.noreply.github.com>
2020-09-02 13:36:53 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-        <div class="col-md-12">
+<div class="row">
+        <div class="col-12 col-md-12">
 
  <form method="POST" action="{{ route('login') }}">
                         @csrf
    <label for="email" class="col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
 
-                         <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                         <input id="email" type="email" class="col-12 form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
    @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -18,7 +18,7 @@
                             <label for="password">{{ __('Password') }}</label>
 
                            
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" type="password" class="col-12 form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
